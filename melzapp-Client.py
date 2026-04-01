5dc96c96e7a1574ff3c9c376e5e665394c2d53ef
hexallen <48969480+hexallen@users.noreply.github.com>
2026-03-31 17:01:10 -0700

    Add files via upload

---
+++ melzapp-Client.py
@@ -0,0 +1,19 @@
+#Quantum Age Publishing presents: Melzapp - My Personal Messenger
+#CLIENT
+#!/usr/bin/env python3
+
+import threading
+import gtk
+import gobject
+import socket
+import re
+import time
+import datetime
+
+gobject.threads_init()
+
+
+
+
+
+
